8a278ad88bc04896c73c0332bee92eb6ef0cb16e
allexandessokollov <64951280+allexandessokollov@users.noreply.github.com>
2020-12-06 16:08:48 +0300

    Update Unit1.cpp

--- Unit1.cpp
+++ Unit1.cpp
@@ -41,6 +41,7 @@ void __fastcall TForm1::enterTextKeyDown(TObject *Sender, WORD &Key,
 int TForm1::countAnswer(String str)
 {
 
+    str += "";
     int counter = 0, tmpCounter = 0, strLength = str.Length();
     String strAnswer;
 
